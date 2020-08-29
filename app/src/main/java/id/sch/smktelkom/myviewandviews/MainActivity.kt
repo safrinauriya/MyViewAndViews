27ce3024a87285a8519102c6ee6641fbacb3bbba
safrinauriya <safrina__auriya_28rpl@student.smktelkom-mlg.sch.id>
2020-08-29 09:15:29 +0700

    first commit

---
+++ app/src/main/java/id/sch/smktelkom/myviewandviews/MainActivity.kt
@@ -0,0 +1,16 @@
+package id.sch.smktelkom.myviewandviews
+
+import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (supportActionBar != null) {
+            (supportActionBar as ActionBar).title = "Google Pixel"
+        }
+    }
+}
